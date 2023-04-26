e9b94dcb0154ffb0ee2cf456b0a38550279eb8b5
Imnamok <106565147+Imnamok@users.noreply.github.com>
2023-04-26 15:49:10 +0900

    clone finish

--- app.js
+++
@@ -1,9 +0,0 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form Button");
-
-function onLoginBtnClick() {
-  console.dir(loginInput);
-  console.log("click!!!!");
-}
-
-loginButton.addEventListener("click", onLoginBtnClick);
